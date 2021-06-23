dacc70410c29489ebd4a2be3d8199873976a4197
i-victor <58788899+i-victor@users.noreply.github.com>
2021-06-23 20:16:37 +0300

    start implement deck selection, stable

--- pvz-js/js/app.js
+++ pvz-js/js/app.js
@@ -11,20 +11,26 @@ $(document).ready(() => {
 
 	const startGame = (level) => {
 			startLevel = level;
-			document.getElementById("canvas1").style.display = 'block';
-			//const level = document.getElementById('levelSelect').value;
-			document.getElementById('lightSlider').style.display = 'none';
 			document.getElementById('play').style.display = 'none';
-			document.getElementById('levelSlider').style.display = 'none';
+			//const level = document.getElementById('levelSelect').value;
+			document.getElementById("container-deckSelect").style.display = 'none';
+			document.getElementById('container-worldSelect').style.display = 'none';
+			document.getElementById('container-levelSlider').style.display = 'none';
+			document.getElementById("game-canvas").style.display = 'block';
 			console.log('World:', startWorld, 'Level:', startLevel);
-			const game = new Game('canvas1', startWorld, startLevel); // run game
+			const game = new Game('game-canvas', startWorld, startLevel); // run game
+	};
+
+	const levelSelect = (level) => {
+		startGame(level);
 	};
 
 	const worldSelect = (world) => {
 		startWorld = world;
-		document.getElementById("lightSlider").style.display = 'none';
-		document.getElementById("levelSlider").style.display = 'block';
-		$("#levelSlider").lightSlider({
+		document.getElementById("container-deckSelect").style.display = 'none';
+		document.getElementById("container-worldSelect").style.display = 'none';
+		document.getElementById("container-levelSlider").style.display = 'block';
+		$("#slider-levelSlider").lightSlider({
 			item: 2,
 			autoWidth: false,
 			slideMove: 1, // slidemove will be 1 if loop is true
@@ -32,10 +38,10 @@ $(document).ready(() => {
 		});
 	};
 
-	$("#lightSlider").lightSlider({
-		item: 2,
+	$("#slider-worldSelect").lightSlider({
+		item: 4,
 		autoWidth: false,
-		slideMove: 1, // slidemove will be 1 if loop is true
+		slideMove: 2, // slidemove will be 1 if loop is true
 		slideMargin: 10,
 	});
 
@@ -88,19 +94,19 @@ $(document).ready(() => {
 	});
 
 	$('#game-level-21').on('click', () => {
-		startGame(1);
+		levelSelect(1);
 	});
 
 	$('#game-level-22').on('click', () => {
-		startGame(2);
+		levelSelect(2);
 	});
 
 	$('#game-level-23').on('click', () => {
-		startGame(3);
+		levelSelect(3);
 	});
 
 	$('#game-level-24').on('click', () => {
-		startGame(4);
+		levelSelect(4);
 	});
 
 });
